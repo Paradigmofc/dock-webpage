b82a1e98794b48499891eca844c5225dac923fd5
Alexey <paradigmofc@gmail.com>
2022-01-31 17:44:12 +0200

    fixes

--- js/main.js
+++ js/main.js
@@ -14,37 +14,3 @@ btn.on('click', function(e) {
   e.preventDefault();
   $('html, body').animate({scrollTop:0}, '100');
 });
-
-// $(document).ready(function(){
-//   animateDiv('.pin1');
-//   animateDiv('.pin2');
-//   animateDiv('.pin3');
-//   animateDiv('.pin4');
-//   animateDiv('.pin5');
-//   animateDiv('.pin6');
-//   animateDiv('.pin7');
-//   animateDiv('.pin8');
-//   animateDiv('.pin9');
-//   animateDiv('.pin10');
-// });
-
-// // function makeNewPosition(){
-  
-// //   // Get viewport dimensions (remove the dimension of the div)
-// //   var h = $(window).height() - 500;
-// //   var w = $(window).width() - 500;
-  
-// //   var nh = Math.floor(Math.random() * h);
-// //   var nw = Math.floor(Math.random() * w);
-  
-// //   return [nh,nw];    
-  
-// // }
-
-// // function animateDiv(myclass){
-// //   var newq = makeNewPosition();
-// //   $(myclass).animate({ top: newq[0], left: newq[1] }, 10000,   function(){
-// //     animateDiv(myclass);        
-// //   });
-  
-// // };
